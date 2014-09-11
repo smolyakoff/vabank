10bf5869e8e23d184fed931048ce858b5eb1e20e
Zakhar Makarevich <zakhar_makarevich@epam.com>
2014-09-11 13:24:14 +0300

    OWIN Test.

---
+++ src/VaBank.UI.Web/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin;
+
+namespace VaBank.UI.Web
+{    
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.Use(new Func<AppFunc, AppFunc>(next => (AppFunc)Invoke));            
+        }
+
+        public Task Invoke(IDictionary<string, object> env)
+        {
+            var response = new OwinResponse(env);
+            return response.WriteAsync("Hello, World!");
+        }
+    }
+}
