fdb61cbe8c153d96849bd051cfe4cf44cdb84077
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2014-09-16 15:11:06 +0300

    Testing ci build.
    Related Work Items: #24

--- deploy/build.ps1
+++ deploy/build.ps1
@@ -6,7 +6,7 @@ $FailureColor = "Red"
 
 Import-Module -Name "$SolutionDir/tools/Invoke-MsBuild.psm1"
 $LogFilePath = Invoke-MsBuild -Path "$SolutionDir/deploy/VaBank.proj"-GetLogPath
-$BuildSuccessful = Invoke-MsBuild -Path "$SolutionDir/deploy/VaBank.proj" -P "/t:Migrate /v:normal"  -KeepBuildLogOnSuccessfulBuilds -AutoLaunchBuildLogOnFailure -ShowBuildWindowAndPromptForInputBeforeClosing
+$BuildSuccessful = Invoke-MsBuild -Path "$SolutionDir/deploy/VaBank.proj" -P "/t:EnsureDatabaseExists /v:normal" -AutoLaunchBuildLogOnFailure -ShowBuildWindowAndPromptForInputBeforeClosing
 if ($BuildSuccessful) {
 	Write-Host -ForegroundColor $SuccessColor -Object "Build was successfull. Log file path: [$LogFilePath]"
 } else {
