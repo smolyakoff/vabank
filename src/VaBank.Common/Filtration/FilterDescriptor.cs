786013d98d096d1e2fbf28d40d08412c8873eb49
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-09-24 12:09:16 +0300

    ToExpression method refactored. Combiner filter building added.

--- src/VaBank.Common/Filtration/FilterDescriptor.cs
+++ src/VaBank.Common/Filtration/FilterDescriptor.cs
@@ -9,29 +9,31 @@ using System.Threading.Tasks;
 namespace VaBank.Common.Filtration
 {
     public class FilterDescriptor
-    {        
+    {
+        private const string ParamName = "o";
+
         public Filter Context { get; set; }
 
         public Expression<Func<T, bool>> ToExpression<T>()
         {
-            Expression<Func<T, bool>> expr;
+            Expression body;
 
             switch (Context.Type)
             {
                 case FilterType.Combiner:
-                    expr = BuildCombinerFilter((CombinerFilter)Context);
+                    body = BuildCombinerFilter<T>((CombinerFilter)Context);
                     break;
                 case FilterType.Expression:
-                    expr = BuildExpressionFilter(((ExpressionFilter)Context));
+                    body = BuildExpressionFilter<T>(((ExpressionFilter)Context));
                     break;
                 default:
                     throw new InvalidOperationException();
             }
 
-            return expr;
+            return CompleteBuild<T>(body);
         }
 
-        private Expression<Func<T, bool>> BuildExpressionFilter(ExpressionFilter filter)
+        private Expression BuildExpressionFilter<T>(ExpressionFilter filter)
         {
             Expression body, left, right, param;
 
@@ -44,7 +46,7 @@ namespace VaBank.Common.Filtration
                     propName = property.Name;
             }
 
-            param = Expression.Parameter(type, "f");            
+            param = Expression.Parameter(type, ParamName);            
             left = Expression.Property(param, propName);
             right = Expression.Constant(filter.Value);
 
@@ -77,55 +79,80 @@ namespace VaBank.Common.Filtration
                     break;
 
                 case FilterOperator.In:
-                    methodInfo = typeof(string).GetRuntimeMethod("Contains", typeof(string));
+                    methodInfo = typeof(string).GetRuntimeMethod("Contains", new[] { typeof(string) });
                     body = Expression.Call(right, methodInfo, left);
                     break;
 
                 case FilterOperator.NotIn:
-                    methodInfo = typeof(string).GetRuntimeMethod("Contains", typeof(string));
+                    methodInfo = typeof(string).GetRuntimeMethod("Contains", new[] { typeof(string) });
                     body = Expression.Not(Expression.Call(right, methodInfo, left));
                     break;
 
                 case FilterOperator.StartsWith:
-                    methodInfo = typeof(string).GetRuntimeMethod("StartsWith", typeof(string));
+                    methodInfo = typeof(string).GetRuntimeMethod("StartsWith", new[] { typeof(string) });
                     body = Expression.Call(left, methodInfo, right);
                     break;
 
                 case FilterOperator.NotStartsWith:
-                    methodInfo = typeof(string).GetRuntimeMethod("StartsWith", typeof(string));
+                    methodInfo = typeof(string).GetRuntimeMethod("StartsWith", new[] { typeof(string) });
                     body = Expression.Not(Expression.Call(left, methodInfo, right));
                     break;
 
                 case FilterOperator.EndsWith:
-                    methodInfo = typeof(string).GetRuntimeMethod("EndsWith", typeof(string));
+                    methodInfo = typeof(string).GetRuntimeMethod("EndsWith", new[] { typeof(string) });
                     body = Expression.Call(left, methodInfo, right);
                     break;
 
                 case FilterOperator.NotEndsWith:
-                    methodInfo = typeof(string).GetRuntimeMethod("EndsWith", typeof(string));
+                    methodInfo = typeof(string).GetRuntimeMethod("EndsWith", new[] { typeof(string) });
                     body = Expression.Not(Expression.Call(left, methodInfo, right));
                     break;
 
                 case FilterOperator.Contains:
-                    methodInfo = typeof(string).GetRuntimeMethod("Contains", typeof(string));
+                    methodInfo = typeof(string).GetRuntimeMethod("Contains", new[] { typeof(string) });
                     body = Expression.Call(left, methodInfo, right);
                     break;
 
                 case FilterOperator.NotContains:
-                    methodInfo = typeof(string).GetRuntimeMethod("Contains", typeof(string));
+                    methodInfo = typeof(string).GetRuntimeMethod("Contains", new[] { typeof(string) });
                     body = Expression.Not(Expression.Call(left, methodInfo, right));
                     break;
 
                 default:
                     throw new InvalidOperationException();
             }
-            
-            return Expression.Lambda<Func<T, bool>>(body, param);
+
+            return body;
         }
 
-        private Expression<Func<T, bool>> BuildCombinerFilter(CombinerFilter filter)
+        private Expression BuildCombinerFilter<T>(CombinerFilter filter)
         {
+            Expression body = null;
+            foreach (var item in filter.Filters)
+            {
+                Expression expr = null;
+                if (item is ExpressionFilter)
+                    expr = BuildExpressionFilter<T>((ExpressionFilter)item);
+                if (item is CombinerFilter)
+                    expr = BuildCombinerFilter<T>((CombinerFilter)item);
+                switch (filter.Logic)
+                {
+                    case FilterLogic.And:
+                        body = Expression.And(body, expr);
+                        break;
+                    case FilterLogic.Or:
+                        body = Expression.Or(body, expr);
+                        break;
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+            return body;
+        }
 
+        private Expression<Func<T, bool>> CompleteBuild<T>(Expression body)
+        {
+            return Expression.Lambda<Func<T, bool>>(body, Expression.Parameter(typeof(T), ParamName));
         }
     }
 }
