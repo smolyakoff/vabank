f60bcd9b20cf8c572cc3b47c5e34fc266de6929e
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2014-09-25 23:57:53 +0300

    Added empty filter converter to deserializer from string.

---
+++ src/VaBank.Common/Filtration/Serialization/FilterConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace VaBank.Common.Filtration.Serialization
+{
+    //converter from string: useful for get requests
+    public class FilterConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var stringValue = value as string;
+            if (string.IsNullOrEmpty(stringValue))
+            {
+                return base.ConvertFrom(context, culture, value);
+            }
+            return Parse(stringValue);
+        }
+
+        private Filter Parse(string filterString)
+        {
+            return null;
+        }
+    }
+}
