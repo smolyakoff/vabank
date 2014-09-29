5fa2c8afdcfa68411d75cfe4d5a0a8ef88f3196a
Vladislav Zhukouski <Vladisalv_Zhukouski@epam.com>
2014-09-30 00:36:48 +0300

    LogMap file added.

---
+++ src/VaBank.Data.EntityFramework/Mappings/LogMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaBank.Core.Entities;
+
+namespace VaBank.Data.EntityFramework.Mappings
+{
+    public class LogMap: EntityTypeConfiguration<Log>
+    {
+        public LogMap()
+        {
+            ToTable("Logs");
+            HasKey(t => t.Id);
+            HasRequired<string>(t => t.Application);
+            HasRequired<string>(t => t.Level);
+            Property(t => t.TimeStampUtc).IsRequired();
+            HasRequired<string>(t => t.Type);
+            HasRequired<string>(t => t.User);
+            HasRequired<string>(t => t.Message);
+            HasRequired<string>(t => t.Exception);
+            HasRequired<string>(t => t.Source);
+
+        }
+    }
+}
