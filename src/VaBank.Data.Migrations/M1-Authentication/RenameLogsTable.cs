a680e28fabec362e4cd92cd680daebd90206e919
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2014-10-02 14:40:59 +0300

    Renamed Logs table to SystemLog.

---
+++ src/VaBank.Data.Migrations/M1-Authentication/RenameLogsTable.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace VaBank.Data.Migrations
+{
+    [Migration(3, "Renamed Logs table to SystemLog")]
+    [Tags("Maintenance", "Development", "Production")]
+    public class RenameLogsTable : Migration
+    {
+        public override void Up()
+        {
+            Rename.Table("Logs").InSchema("Maintenance").To("SystemLog");
+        }
+
+        public override void Down()
+        {
+            Rename.Table("SystemLog").InSchema("Maintenance").To("Logs");
+        }
+    }
+}
