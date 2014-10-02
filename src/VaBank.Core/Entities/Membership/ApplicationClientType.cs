63733efb60baf60c2707624b7405921d38bf9eca
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-10-02 14:07:07 +0300

    Membership entities added.

---
+++ src/VaBank.Core/Entities/Membership/ApplicationClientType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VaBank.Core.Entities.Membership
+{
+    public enum ApplicationClientType : byte
+    {
+        JavaScript,
+        NativeConfidential
+    }
+}
