029691cf67564a12d66e2795762691d98a5d91dd
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-10-03 11:55:11 +0300

    Constants for mapping added.

---
+++ src/VaBank.Data.EntityFramework/Mappings/RestrictionConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace VaBank.Data.EntityFramework.Mappings
+{
+    internal static class RestrictionConstants
+    {
+        public const int ShortNameLength = 50;
+        public const int EmailLength = 50;
+        public const int PhoneNumberLength = 30;
+        public const int BigStringLength = 1024;
+        public const int SecurityStringLength = 256;
+    }
+}
