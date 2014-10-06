df79ff211134b5c826fbc9156dbec649e6feae2a
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2014-10-06 16:17:54 +0300

    Added migration to seed users.

---
+++ src/VaBank.Data.Migrations/M1-Authentication/M1User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using VaBank.Common.Security;
+
+namespace VaBank.Data.Migrations
+{
+    internal class M1User
+    {
+        public static M1User Create(string login, string password, string role)
+        {
+            var user = new M1User
+            {
+                UserId = Guid.NewGuid(),
+                UserName = login,
+                Password = Password.Create(password),
+                Claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, login),
+                    new Claim(ClaimTypes.Role, role)
+                }
+            };
+            var credentials = new List<string>
+            {
+                login,
+                user.Password.PasswordHash,
+            };
+            credentials.AddRange(user.Claims.OrderBy(x => x.Type).Select(x => x.ToString()));
+            user.SecurityStamp = Hash.Compute(credentials);
+            return user;
+        }
+
+        public Guid UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public Password Password { get; set; }
+
+        public string SecurityStamp { get; set; }
+
+        public IList<Claim> Claims { get; set; }
+
+        public object ToTableRow()
+        {
+            return new
+            {
+                UserId,
+                UserName,
+                Password.PasswordHash,
+                Password.PasswordSalt,
+                SecurityStamp,
+                LockoutEnabled = false,
+                AccessFailedCount = 0,
+                Deleted = false,
+            };
+        }
+    }
+}
---
+++ src/VaBank.Data.Migrations/M1-Authentication/SeedUsers.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using FluentMigrator;
+
+namespace VaBank.Data.Migrations
+{
+    [Migration(5, "Some users for testing.")]
+    [Tags("Development", "Test", "Production")]
+    public class SeedUsers : Migration
+    {
+        private const string DevPassword = "vabank2014!";
+
+        public override void Up()
+        {
+            var users = new List<M1User>()
+            {
+                M1User.Create("smolyakoff", DevPassword, "Admin"),
+                M1User.Create("losik", DevPassword, "Admin"),
+                M1User.Create("sherbetr", DevPassword, "Admin"),
+                M1User.Create("jaymz", DevPassword, "Admin"),
+                M1User.Create("gostop", DevPassword, "Admin"),
+                M1User.Create("customer1", DevPassword, "Customer"),
+                M1User.Create("customer2", DevPassword, "Customer"),
+                M1User.Create("customer3", DevPassword, "Customer"),
+            };
+            users.ForEach(x =>
+            {
+                InsertUser(x);
+                InsertClaims(x);
+            });
+        }
+
+        private void InsertUser(M1User user)
+        {
+            Insert.IntoTable("User").InSchema("Membership").Row(user.ToTableRow());
+        }
+
+        private void InsertClaims(M1User user)
+        {
+            foreach (var claim in user.Claims)
+            {
+                Insert.IntoTable("UserClaim").InSchema("Membership").Row(new
+                {
+                    UserId = user.UserId,
+                    Type = claim.Type,
+                    Value = claim.Value
+                });
+            }
+        }
+
+        public override void Down()
+        {
+            //data migration, not used
+        }
+    }
+}
