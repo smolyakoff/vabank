b3c335d40b98152ce7af0bcb6d61c876cfdb661d
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2014-10-07 21:10:09 +0300

    Build fixed.

--- src/VaBank.Services.Contracts/Membership/ApplicationClientModel.cs
+++ src/VaBank.Services.Contracts/Membership/ApplicationClientModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VaBank.Services.Contracts.Membership
+﻿namespace VaBank.Services.Contracts.Membership
 {
-    class ApplicationClientModel
+    public class ApplicationClientModel
     {
     }
 }
