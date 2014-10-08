6234fd6009d319a68751b165ae220b63e61445fa
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2014-10-08 16:49:36 +0300

    Playing with owin and oauth.

---
+++ src/VaBank.UI.Web/Api/Infrastructure/Auth/VabankRefreshTokenProvider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin.Security.Infrastructure;
+
+namespace VaBank.UI.Web.Api.Infrastructure.Auth
+{
+    public class VabankRefreshTokenProvider : AuthenticationTokenProvider
+    {
+        public override void Create(AuthenticationTokenCreateContext context)
+        {
+            //here is should create refresh token in the db
+            base.Create(context);
+        }
+    }
+}
