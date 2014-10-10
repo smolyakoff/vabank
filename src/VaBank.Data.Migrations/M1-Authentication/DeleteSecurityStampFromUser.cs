b44852c1edf62be9c1d3e4060b55ca5593aae91e
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-10-10 11:20:35 +0300

    Delete SecurityStamp column from User table migration.

---
+++ src/VaBank.Data.Migrations/M1-Authentication/DeleteSecurityStampFromUser.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace VaBank.Data.Migrations
+{
+    [Migration(12, "Delete SecurityStamp column from User table")]
+    public class DeleteSecurityStampFromUser : Migration
+    {
+        public override void Down()
+        {
+            Create.Column("SecurityStamp").OnTable("User").InSchema("Membership").AsSecurityString().NotNullable();
+        }
+
+        public override void Up()
+        {
+            Delete.Column("SecurityStamp").FromTable("User").InSchema("Membership");
+        }
+    }
+}
