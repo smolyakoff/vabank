ed33285d52a15eb3d1d95a6106ceebc3b89a828b
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2014-10-21 00:38:27 +0300

    Started ui for audit log.

---
+++ src/VaBank.UI.Web/Client/app/areas/admin/audit-log/audit-log-ctrl.js
@@ -0,0 +1,53 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('vabank.webapp');
+    app.controller('auditLogController', auditLog);
+    
+    auditLog.$inject = ['$scope', 'uiTools', 'auditLogService'];
+
+    function auditLog($scope, uiTools, auditLogService) {
+        var multiselect = uiTools.control.multiselect;
+        var LogEntry = auditLogService.LogEntry;
+
+        $scope.filter = angular.copy(LogEntry.defaults.filter);
+
+        $scope.loading = uiTools.promiseTracker();
+
+        $scope.lookup = {            
+          codes: multiselect.getSelectChoices(['LOGIN', 'UPDATE-USER'])
+        };
+
+        $scope.logs = [
+            {
+                operationId: 'abc',
+                startedUtc: new Date(),
+                userName: 'terminator',
+                applicationId: 'vabank.webapp',
+                appActions: [
+                    { code: 'LOGIN', description: 'User logged in', jsonData: '{ "abc": "abc"}' },
+                    { code: 'LOGIN', description: 'User logged in', jsonData: '{ "abc": "abc"}' },
+                    { code: 'LOGIN', description: 'User logged in', jsonData: '{ "abc": "abc"}' }
+                ]
+            },
+            {
+                operationId: 'abc',
+                startedUtc: new Date(),
+                userName: 'terminator',
+                applicationId: 'vabank.webapp',
+                appActions: [
+                    { code: 'LOGIN', description: 'User logged in', jsonData: '{ "abc": "abc"}' },
+                ]
+            }
+        ];
+
+        $scope.displayedLogs = [].concat($scope.logs);
+
+        $scope.show = function() {
+
+        };
+
+    }
+
+
+})();
