4293b7e88461f903dcc25305d878c2ddfa8e3e13
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-10-21 11:24:56 +0300

    Container initializer helper.

--- tests/VaBank.Data.Tests/VaBankContextTests.cs
+++ tests/VaBank.Data.Tests/VaBankContextTests.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VaBank.Core.App;
 using VaBank.Core.Membership;
 using VaBank.Data.EntityFramework;
@@ -10,58 +11,58 @@ using VaBank.Data.EntityFramework.App;
 namespace VaBank.Data.Tests
 {    
     [TestClass]
-    public class VaBankContextTests
+    public class VaBankContextTests : BaseTest
     {
-        //[TestCategory("Development")]
-        //[TestMethod]
-        //public void Can_VaBank_Context_Save_User()
-        //{
-        //    var context = new VaBank.Data.EntityFramework.VaBankContext();
+        [TestCategory("Development")]
+        [TestMethod]
+        public void Can_VaBank_Context_Save_User()
+        {
+            var context = Container.Resolve<VaBankContext>();
 
-        //    var user = User.Create(string.Format("Test_{0}", Guid.NewGuid().ToString()), "Admin", "1234567890!");
-        //    var profile = new UserProfile(user.Id)
-        //    {
-        //        Email = "test@test.com",
-        //        FirstName = "Test",
-        //        LastName = "Test",
-        //        PhoneNumber = "+375 29 3981330",
-        //        PhoneNumberConfirmed = true,
-        //        SecretPhrase = "Test",
-        //        SmsConfirmationEnabled = false,
-        //        SmsNotificationEnabled = false                
-        //    };
+            var user = User.Create(string.Format("Test_{0}", Guid.NewGuid().ToString()), "Admin", "1234567890!");
+            var profile = new UserProfile(user.Id)
+            {
+                Email = "test@test.com",
+                FirstName = "Test",
+                LastName = "Test",
+                PhoneNumber = "+375 29 3981330",
+                PhoneNumberConfirmed = true,
+                SecretPhrase = "What is the color of night?",
+                SmsConfirmationEnabled = false,
+                SmsNotificationEnabled = false
+            };
 
-        //    context.Set<User>().Add(user);
-        //    context.Set<UserProfile>().Add(profile);                        
-        //    context.SaveChanges();
-        //}
+            context.Set<User>().Add(user);
+            context.Set<UserProfile>().Add(profile);
+            context.SaveChanges();
+        }
 
-        //[TestCategory("Development")]
-        //[TestMethod]
-        //public void Can_Start_And_Finish_Operations()
-        //{
-        //    var context = new VaBankContext();
-        //    var repo = new OperationRepository(context);
-        //    using (var transaction = context.Database.BeginTransaction())
-        //    {
+        [TestCategory("Development")]
+        [TestMethod]
+        public void Can_Start_And_Finish_Operations()
+        {
+            var context = Container.Resolve<VaBankContext>();
+            var repo = new OperationRepository(context);
+            using (var transaction = context.Database.BeginTransaction())
+            {
 
-        //        var op1 = repo.Start("TEST", null);
+                var op1 = repo.Start("TEST", null);
 
-        //        var op2 = repo.GetCurrent();
-        //        var op3 = repo.GetCurrent();
+                var op2 = repo.GetCurrent();
+                var op3 = repo.GetCurrent();
 
-        //        var ops = new List<Operation>() { op1, op2, op3 };
-        //        Assert.AreEqual(1, ops.Select(x => x.Id).Distinct().Count());
+                var ops = new List<Operation>() { op1, op2, op3 };
+                Assert.AreEqual(1, ops.Select(x => x.Id).Distinct().Count());
 
-        //        repo.Stop(op1);
+                repo.Stop(op1);
 
-        //        var op4 = repo.GetCurrent();
-        //        Assert.IsNull(op4);
-        //        transaction.Commit();
-        //    }
+                var op4 = repo.GetCurrent();
+                Assert.IsNull(op4);
+                transaction.Commit();
+            }
 
-        //    var op5 = repo.GetCurrent();
-        //    Assert.IsNull(op5);
-        //}
+            var op5 = repo.GetCurrent();
+            Assert.IsNull(op5);
+        }
     }
 }
