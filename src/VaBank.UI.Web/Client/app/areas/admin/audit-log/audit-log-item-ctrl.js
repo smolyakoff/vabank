b9a2a32d237aa996094a26aeed1eebb6ad34951c
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2014-10-21 16:50:03 +0300

    Added database view to audit-log screen.

--- src/VaBank.UI.Web/Client/app/areas/admin/audit-log/audit-log-item-ctrl.js
+++ src/VaBank.UI.Web/Client/app/areas/admin/audit-log/audit-log-item-ctrl.js
@@ -8,8 +8,37 @@
 
     function auditLogItem($scope, uiTools, auditLogService) {
         var item = $scope.log;
-        
-    }
+        var transition = {
+            'app': 'db',
+            'db': 'app'
+        };
 
+        $scope.view = 'app';
+        $scope.detailedLog = {
+            dbActions: [
+                {
+                    tableName: '[Membership].[User]',
+                    changes: [
+                        {
+                            action: 'Insert',
+                            timestampUtc: new Date(),
+                            values: { id: 213, userName: 'name', age: 15 }
+                        },
+                        {
+                            action: 'Update',
+                            timestampUtc: new Date(),
+                            values: { id: 213, userName: 'name', age: 15 }
+                        },
+                    ]
+                }
+            ]
+        };
+
+        $scope.switchView = function () {
+            var currentView = $scope.view;
+            $scope.view = transition[currentView];
+
+        };
+    }
 
 })();
