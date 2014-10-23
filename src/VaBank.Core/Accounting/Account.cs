aee72c8adaa377de527f4fdcc9c30377bf656633
Vladislav Zhukouski <Vladislav_Zhukouski@epam.com>
2014-10-23 12:43:41 +0300

    Accounting entities updated.

--- src/VaBank.Core/Accounting/Account.cs
+++ src/VaBank.Core/Accounting/Account.cs
@@ -9,25 +9,16 @@ namespace VaBank.Core.Accounting
 {
     public class Account: Entity
     {
-        public CardVendor CardVendor { get; set; }
-
-        public string CardNo { get; set; }
-
-        public string CardholderFirstName { get; set; }
-
-        public string CardholderLastName { get; set; }
-
-        public DateTime CardExpirationDateUtc { get; set; }
-
-        public DateTime AccountOpenedDateUtc { get; set; }
-
-        public DateTime AccountExpirationDateUtc
+        public Account(string accountNo)
         {
-            get { return CardExpirationDateUtc; }
+
         }
 
-        public CardLimits CardLimits { get; set; }
-
-        public byte[] RowVersion { get; set; }
+        public string AccountNo { get; set; }
+        public Currency Currency { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime OpenDateUtc { get; set; }
+        public DateTime ExpirationDateUtc { get; set; }
+        public string Type { get; set; }
     }
 }
