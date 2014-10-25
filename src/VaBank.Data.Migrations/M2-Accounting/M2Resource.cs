f662dee7ac919df31a0777efd7daf1c406ff4547
Natalia Shumilova <sherbetr@gmail.com>
2014-10-25 17:59:03 +0300

    Add card vendors to database

---
+++ src/VaBank.Data.Migrations/M2-Accounting/M2Resource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VaBank.Data.Migrations
+{
+    internal class M2Resource
+    {
+        public static M2Resource CreateWebServerFile(string uri)
+        {
+            return new M2Resource
+            {
+                Id = Guid.NewGuid(),
+                Location = "WebServer",
+                Type = "FileLink",
+                Uri = uri
+            };
+        }
+
+        public Guid Id { get; private set; }
+
+        public string Type { get; private set; }
+
+        public string Uri { get; private set; }
+
+        public string Location { get; private set; }
+    }
+}
