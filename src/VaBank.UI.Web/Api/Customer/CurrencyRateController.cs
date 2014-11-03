ab202682b73e14da33b9a86a329c68e5a0f41059
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-11-03 12:03:53 +0300

    CurrencyRateController

---
+++ src/VaBank.UI.Web/Api/Customer/CurrencyRateController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using VaBank.Services.Contracts.Processing;
+
+namespace VaBank.UI.Web.Api.Customer
+{
+    [RoutePrefix("api/currencies/rates")]
+    public class CurrencyRateController : ApiController
+    {
+        private readonly ICurrencyRateService _currencyRateService;
+
+        [HttpGet]
+        [Route]
+        public IHttpActionResult GetAllRates()
+        {
+            return Ok(_currencyRateService.GetAllRates());
+        }
+    }
+}
