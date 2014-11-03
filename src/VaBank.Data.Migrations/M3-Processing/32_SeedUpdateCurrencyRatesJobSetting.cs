e43a1d756cb610ea2bb56b9292bee202d40a98f6
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-11-03 13:43:51 +0300

    Prepare UpdateCurrencyRatesJob setting migration.

---
+++ src/VaBank.Data.Migrations/M3-Processing/32_SeedUpdateCurrencyRatesJobSetting.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+using Newtonsoft.Json;
+
+namespace VaBank.Data.Migrations.M3_Processing
+{
+    [Migration(32, "Seed UpdateCurrencyRates job setting")]
+    [Tags("Development", "Test", "Production")]
+    public class SeedUpdateCurrencyRatesJobSetting : Migration
+    {
+        private const string SettingKey = "VaBank.Jobs.{0}";
+
+        public override void Down()
+        {
+        }
+
+        public override void Up()
+        {
+            //TODO: Invoke insert method
+        }
+
+        private object UpdateCurrencyRates()
+        {
+            //TODO: Fill real cron expression
+            var config = new { CronExpression = "" };
+            var json = JsonConvert.SerializeObject(limits);
+            var node = JsonConvert.DeserializeXNode(json, "Setting");
+            return new { Key = string.Format(SettingKey, "UpdateCurrencyRates"), Value = node.ToString() };
+        }
+    }
+}
