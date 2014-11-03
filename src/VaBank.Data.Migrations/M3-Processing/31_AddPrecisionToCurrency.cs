abd6f8085688e7d16542125488c239510cee3560
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-11-03 12:35:26 +0300

    Currency precision column.

---
+++ src/VaBank.Data.Migrations/M3-Processing/31_AddPrecisionToCurrency.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace VaBank.Data.Migrations
+{
+    [Migration(31, "Add precision column to [Accounting].[Currency] table.")]
+    [Tags("Development", "Test", "Production")]
+    public class AddPrecisionToCurrency : Migration
+    {
+        public override void Down()
+        {            
+        }
+
+        public override void Up()
+        {
+            Alter.Table("Currency").InSchema("Accounting").AddColumn("Precision").AsInt32().NotNullable().WithDefaultValue(2);
+            Update.Table("Currency").InSchema("Accounting").Set(new { Precision = 0 }).Where(new { CurrencyISOName = "BYR" });
+        }
+    }
+}
