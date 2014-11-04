bdba2b6806adb3aa9c270e9cddec9a29043f2c3f
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2014-11-04 20:13:47 +0300

    Commited missing files.

---
+++ src/VaBank.Services/Infrastructure/InfrastructureProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using VaBank.Core.App.Entities;
+using VaBank.Services.Contracts.Infrastructure.Secuirty;
+
+namespace VaBank.Services.Infrastructure
+{
+    public class InfrastructureProfile : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<SecurityCode, SecurityCodeTicketModel>();
+        }
+    }
+}
