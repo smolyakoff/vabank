197531543e96e9accff90d3d19e0ef04207c7cb8
Vladislav Zhukouski <Vladisalv_Zhukouski@epam.com>
2014-11-04 03:15:01 +0300

    ExchangeRate table - in progress

---
+++ src/VaBank.Data.Migrations/M3-Processing/34_AddExchangeRateTable.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaBank.Data.Migrations
+{
+    [Migration(34, "Add ExchangeRate table to [Accounting] schema.")]
+    [Tags("Development", "Test", "Production")]
+    public class AddExchangeRateTable : Migration
+    {
+        private const string SchemaName = "Accounting";
+        public override void Down()
+        {
+            Delete.Table("ExchangeRate").InSchema(SchemaName);
+        }
+
+        public override void Up()
+        {
+            Create.Table("ExchangeRate").InSchema(SchemaName)
+                .WithColumn("ExchangeRateID").AsGuid().PrimaryKey("PK_Currency")
+                .WithColumn("FromCurrencyISOName").AsCurrencyISOName().ForeignKey("FK_ExchangeRate_To_FromCurrency", SchemaName, "Currency", "CurrencyISOName")
+                .WithColumn("ToCurrencyISOName").AsCurrencyISOName().ForeignKey("FK_ExchangeRate_To_ToCurrency", SchemaName, "Currency", "CurrencyISOName")
+                .WithColumn("BuyRate").AsDecimal().NotNullable()
+                .WithColumn("SellRate").AsDecimal().NotNullable()
+                .WithColumn("TimeStampUtc").AsDateTime().NotNullable()
+                .WithColumn("IsActual").AsBoolean().NotNullable().Indexed("IX_IsActual");
+        }
+    }
+}
