aa8593fc4917a4312c7d6dc098f032291becd8a7
Vladislav Zhukouski <Vladislav_Zhukouski@epam.com>
2014-11-05 13:10:12 +0300

    ExchangeRate table migration.

--- src/VaBank.Data.Migrations/M3-Processing/34_AddExchangeRateTable.cs
+++ src/VaBank.Data.Migrations/M3-Processing/34_AddExchangeRateTable.cs
@@ -2,11 +2,11 @@
 
 namespace VaBank.Data.Migrations
 {
-    [Migration(34, "Add ExchangeRate table to [Processing] schema.")]
+    [Migration(34, "Add ExchangeRate table to [Accounting] schema.")]
     [Tags("Development", "Test", "Production")]
     public class AddExchangeRateTable : Migration
     {
-        private const string SchemaName = "Processing";
+        private const string SchemaName = "Accounting";
         public override void Down()
         {
             Delete.Table("ExchangeRate").InSchema(SchemaName);
@@ -14,15 +14,16 @@ namespace VaBank.Data.Migrations
 
         public override void Up()
         {
-            Create.Schema(SchemaName);
             Create.Table("ExchangeRate").InSchema(SchemaName)
-                .WithColumn("ExchangeRateID").AsGuid().PrimaryKey("PK_Currency")
+                .WithColumn("ExchangeRateID").AsGuid().PrimaryKey("PK_ExchangeRate")
                 .WithColumn("FromCurrencyISOName").AsCurrencyISOName().ForeignKey("FK_ExchangeRate_To_FromCurrency", "Accounting", "Currency", "CurrencyISOName")
                 .WithColumn("ToCurrencyISOName").AsCurrencyISOName().ForeignKey("FK_ExchangeRate_To_ToCurrency", "Accounting", "Currency", "CurrencyISOName")
                 .WithColumn("BuyRate").AsDecimal().NotNullable()
                 .WithColumn("SellRate").AsDecimal().NotNullable()
                 .WithColumn("TimeStampUtc").AsDateTime().NotNullable()
                 .WithColumn("IsActual").AsBoolean().NotNullable().Indexed("IX_IsActual");
+
+            Execute.Sql("ALTER TABLE [Accounting].[ExchangeRate] ADD CONSTRAINT UQ_FromToCurrencyTimeStamp UNIQUE(FromCurrencyISOName, ToCurrencyISOName, TimeStampUtc);");
         }
     }
 }
--- src/VaBank.Data.Migrations/M3-Processing/AddConstraintToExchangeRate.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE [Accounting].[ExchangeRate]
-	ADD CONSTRAINT UQ_FromToCurrencyTimeStamp 
-		UNIQUE(FromCurrencyISOName, ToCurrencyISOName, TimeStampUtc);
