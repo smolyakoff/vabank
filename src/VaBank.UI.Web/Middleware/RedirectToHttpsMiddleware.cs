a67cecf5a4e4d61c1faa50ef2f0b65f9c5413e7a
Vladislav Zhukouski <Vladisalv_Zhukouski@epam.com>
2014-11-13 00:56:47 +0300

    Redirect middleware added - draft.

---
+++ src/VaBank.UI.Web/Middleware/RedirectToHttpsMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace VaBank.UI.Web.Middleware
+{
+    public class RedirectToHttpsMiddleware: OwinMiddleware
+    {
+        private readonly OwinMiddleware _next;
+
+        public RedirectToHttpsMiddleware(OwinMiddleware next)
+            : base(next)
+        {
+            _next = next;
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            if (!context.Request.IsSecure)
+            {
+                var secureUri = context.Request.Uri.ToString().Replace("http", "https");
+                context.Response.Redirect(secureUri);
+            }
+
+            return _next.Invoke(context);
+        }
+    }
+}
