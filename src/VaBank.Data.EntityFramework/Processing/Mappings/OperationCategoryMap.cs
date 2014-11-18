85509279da3d9e7231378f8826a24ab762317ed9
Vladislav Zhukouski <Vladislav_Zhukouski@epam.com>
2014-11-18 17:59:07 +0300

    Processing mappings - draft.

---
+++ src/VaBank.Data.EntityFramework/Processing/Mappings/OperationCategoryMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaBank.Core.Processing.Entities;
+using VaBank.Data.EntityFramework.Common;
+
+namespace VaBank.Data.EntityFramework.Processing.Mappings
+{
+    public class OperationCategoryMap: EntityTypeConfiguration<OperationCategory>
+    {
+        public OperationCategoryMap()
+        {
+            ToTable("OperationCategory", "Processing").HasKey(x => x.Code);
+            Property(x => x.Name).HasMaxLength(Restrict.Length.ShortName).IsRequired();
+            Property(x => x.Description).HasMaxLength(Restrict.Length.BigString);
+        }
+    }
+}
