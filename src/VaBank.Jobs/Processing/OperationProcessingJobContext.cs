1461a7040b9ffaf5050979bb38f401b72641e434
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-11-19 12:56:52 +0300

    OperationProcessingJob uses IProcessingService

--- src/VaBank.Jobs/Processing/OperationProcessingJobContext.cs
+++ src/VaBank.Jobs/Processing/OperationProcessingJobContext.cs
@@ -1,15 +1,11 @@
-﻿using VaBank.Common.Data.Repositories;
-using VaBank.Core.Processing.Entities;
-using VaBank.Core.Processing.Processors;
-using VaBank.Jobs.Common;
+﻿using VaBank.Jobs.Common;
+using VaBank.Services.Contracts.Processing;
 using VaBank.Services.Contracts.Processing.Events;
 
 namespace VaBank.Jobs.Processing
 {
     public class OperationProcessingJobContext : DefaultJobContext<IBankOperationEvent>
     {
-        public IRepository<BankOperation> Operations { get; set; }
-
-        public CentralProcessor Processor { get; set; }
+        public IProcessingService ProcessingService { get; set; }
     }
 }
---
+++ src/VaBank.Jobs/Processing/ProcessingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using VaBank.Services.Contracts.Processing.Commands;
+using VaBank.Services.Contracts.Processing.Events;
+
+namespace VaBank.Jobs.Processing
+{
+    public class ProcessingProfile : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<IBankOperationEvent, ProcessBankOperationCommand>()
+                .ForMember(x => x.BankOperationId, cfg => cfg.MapFrom(x => x.BankOperationId));
+        }
+    }
+}
