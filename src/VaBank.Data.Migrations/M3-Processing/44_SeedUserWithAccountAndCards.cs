3eb98329d8651433bfd066cbd7009a96826b4d50
Vladislav Zhukouski <Vladislav_Zhukouski@epam.com>
2014-11-26 14:18:17 +0300

    Seed Chuck Norris migration added.

---
+++ src/VaBank.Data.Migrations/M3-Processing/44_SeedUserWithAccountAndCards.cs
@@ -0,0 +1,150 @@
+﻿using FluentMigrator;
+using System;
+using System.Data;
+using Dapper;
+
+namespace VaBank.Data.Migrations
+{
+    [Migration(44, "Seed Chuck Norris user with account and card.")]
+    [Tags("Development", "Test", "Production")]
+    public class SeedUserWithAccountAndCards : Migration
+    {
+        private const string DevPassword = "vabank2014!";
+        private const string UserName = "chuck";
+
+        public override void Up()
+        {
+            var user = M3User.Create(UserName, DevPassword, "Customer",
+                new M1Profile()
+                {
+                    Email = "chuck@vabank.com",
+                    FirstName = "Chuck",
+                    LastName = "Norris",
+                    PhoneNumber = "+375296140302",
+                    PhoneNumberConfirmed = true
+                }
+            );
+
+            InsertUser(user);
+            InsertClaims(user);
+            InsertProfile(user);
+
+            Execute.WithConnection((connection, transaction) =>
+            {
+                var nowUtc = DateTime.UtcNow;
+                var expireUtc = nowUtc.AddYears(10);
+                var id = GetUserId(connection, transaction, UserName);
+                var accountNo = "3014" + Seed.RandomStringOfNumbers(9);
+                var account = M2Account.Create(accountNo, "USD", 999999999, nowUtc, expireUtc, "CardAccount");
+                var card1 = M2Card.Create(accountNo, id, UserName);
+                var card2 = M2Card.Create(accountNo, id, UserName);
+                InsertAccount(account, connection, transaction);
+                InsertUserAccount(account.ToUserAccount(id), connection, transaction);
+                InsertCard(card1, connection, transaction);
+                InsertCard(card2, connection, transaction);
+            });
+        }
+
+        private void InsertUser(M3User user)
+        {
+            Insert.IntoTable("User").InSchema("Membership").Row(user.ToTableRow());
+        }
+
+        private void InsertClaims(M3User user)
+        {
+            foreach (var claim in user.Claims)
+            {
+                Insert.IntoTable("UserClaim").InSchema("Membership").Row(new
+                {
+                    UserId = user.UserId,
+                    Type = claim.Type,
+                    Value = claim.Value
+                });
+            }
+        }
+
+        private void InsertProfile(M3User user)
+        {
+            if (user.Profile != null)
+            {
+                Insert.IntoTable("UserProfile").InSchema("Membership").Row(new
+                {
+                    UserId = user.UserId,
+                    FirstName = new ExplicitUnicodeString(user.Profile.FirstName),
+                    LastName = new ExplicitUnicodeString(user.Profile.LastName),
+                    Email = user.Profile.Email,
+                    PhoneNumber = user.Profile.PhoneNumber,
+                    SecretPhrase = new ExplicitUnicodeString(user.Profile.SecretPhrase),
+                    PhoneNumberConfirmed = user.Profile.PhoneNumberConfirmed
+                });
+            }
+        }
+
+        private Guid GetUserId(IDbConnection connection, IDbTransaction transaction, string userName)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.Transaction = transaction;
+                command.CommandText = String.Format("SELECT [UserID] FROM [Membership].[User] WHERE [UserName] = '{0}'", userName);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var id = (Guid)reader["UserID"];
+                        return id;
+                    }
+                }
+            }
+            return Guid.Empty;
+        }
+
+        private void InsertAccount(M2Account account, IDbConnection connection, IDbTransaction transaction)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.Transaction = transaction;
+                command.CommandText = @"INSERT INTO [Accounting].[Account] ([AccountNo], [CurrencyISOName], [Balance], [OpenDateUtc], [ExpirationDateUtc], [Type]) 
+                                    VALUES (@AccountNo, @CurrencyISOName, @Balance, @OpenDateUtc, @ExpirationDateUtc, @Type)";
+                command.CreateSqlParameter("@AccountNo", account.AccountNo);
+                command.CreateSqlParameter("@CurrencyISOName", account.CurrencyISOName);
+                command.CreateSqlParameter("@Balance", SqlDbType.Decimal, account.Balance);
+                command.CreateSqlParameter("@OpenDateUtc", account.OpenDateUtc);
+                command.CreateSqlParameter("@ExpirationDateUtc", account.ExpirationDateUtc);
+                command.CreateSqlParameter("@Type", SqlDbType.NVarChar, account.Type.Text);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void InsertUserAccount(M2UserAccount userAccount, IDbConnection connection, IDbTransaction transaction)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.Transaction = transaction;
+                command.CommandText = "INSERT INTO [Accounting].[User_Account] ([AccountNo], [UserID]) VALUES (@AccountNo, @UserID)";
+                command.CreateSqlParameter("@AccountNo", userAccount.AccountNo);
+                command.CreateSqlParameter("@UserID", userAccount.UserId);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void InsertCard(M2Card card, IDbConnection connection, IDbTransaction transaction)
+        {
+            connection.Execute(
+                "INSERT INTO [Accounting].[Card] ([CardId], [CardNo], [CardVendorId], [HolderFirstName], [HolderLastName], [ExpirationDateUtc])" +
+                "VALUES (@CardId, @CardNo, @CardVendorId, @HolderFirstName, @HolderLastName, @ExpirationDateUtc)",
+                card.ToCard(), transaction);
+            connection.Execute(
+                "INSERT INTO [Accounting].[User_Card_Account] ([CardId], [UserId], [AccountNo]) VALUES (@CardId, @UserId, @AccountNo)",
+                card.ToUserCard(), transaction);
+            connection.Execute(
+                "INSERT INTO [Accounting].[CardSettings] " +
+                "([CardId], [Blocked], [LimitOperationsPerDayLocal], [LimitOperationsPerDayAbroad], [LimitAmountPerDayLocal], [LimitAmountPerDayAbroad])" +
+                "VALUES (@CardId, 0, 20, 10, 1000.0, 500.0)", card, transaction);
+        }
+
+        public override void Down()
+        {
+            //nothing to do
+        }
+    }
+}
---
+++ src/VaBank.Data.Migrations/M3-Processing/M3User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using VaBank.Common.Security;
+
+namespace VaBank.Data.Migrations
+{
+    internal class M3User
+    {
+        public static M3User Create(string login, string password, string role, M1Profile profile = null)
+        {
+            var user = new M3User
+            {
+                UserId = Guid.NewGuid(),
+                UserName = login,
+                Password = Password.Create(password),
+                Claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, login),
+                    new Claim(ClaimTypes.Role, role)
+                },
+                Profile = profile
+            };
+            user.Claims.Add(new Claim(ClaimTypes.Sid, user.UserId.ToString()));
+            var credentials = user.Claims.OrderBy(x => x.Type).Select(x => x.ToString());
+            return user;
+        }
+
+        public Guid UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public Password Password { get; set; }
+
+        public IList<Claim> Claims { get; set; }
+
+        public M1Profile Profile { get; set; }
+
+        public object ToTableRow()
+        {
+            return new
+            {
+                UserId,
+                UserName,
+                Password.PasswordHash,
+                Password.PasswordSalt,
+                LockoutEnabled = false,
+                AccessFailedCount = 0,
+                Deleted = false,
+            };
+        }
+    }
+}
