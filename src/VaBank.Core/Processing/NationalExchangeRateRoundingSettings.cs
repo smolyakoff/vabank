ec71bde8400fad3eed3357804f56157a9866dd09
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-12-02 15:38:29 +0300

    Bug with currency rounding is in process

---
+++ src/VaBank.Core/Processing/NationalExchangeRateRoundingSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using VaBank.Common.Util;
+
+namespace VaBank.Core.Processing
+{
+    [Settings("VaBank.Core.Processing.NationalExchangeRateRoundingSettings")]
+    public class NationalExchangeRateRoundingSettings
+    {
+        public List<NationalExchangeRateRounding> NationalExchangeRateRounding { get; private set; }
+    }
+
+    public class NationalExchangeRateRounding
+    {
+        private NationalExchangeRateRounding()
+        {
+        }      
+
+        public string CurrencyISOName { get; private set; }
+
+        public MoneyRounding Rounding { get; private set; }
+    }
+}
