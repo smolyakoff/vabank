7f57ed47a635b89e1bf1be745d1017d991a664a8
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2014-12-07 22:28:04 +0300

    Added validator stubs for payment forms.

---
+++ src/VaBank.Services/Payments/Forms/CustomPaymentOrderPaymentValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+using Newtonsoft.Json.Linq;
+
+namespace VaBank.Services.Payments.Forms
+{
+    public class CustomPaymentOrderPaymentValidator : AbstractValidator<JObject>
+    {
+        //TODO: write real rules
+    }
+}
