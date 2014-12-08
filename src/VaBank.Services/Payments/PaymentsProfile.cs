93f0e9ddf14e483320ea34dfb8f569fa109ca18a
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-12-09 00:27:02 +0300

    PaymentTemplateModel name constructor

--- src/VaBank.Services/Payments/PaymentsProfile.cs
+++ src/VaBank.Services/Payments/PaymentsProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Text;
+using AutoMapper;
 using Newtonsoft.Json.Linq;
 using VaBank.Core.Payments.Entities;
 using VaBank.Services.Contracts.Payments.Models;
@@ -11,7 +12,24 @@ namespace VaBank.Services.Payments
         {
             CreateMap<PaymentTemplate, PaymentTemplateModel>()
                 .ForMember(x => x.FormTemplate, cfg => cfg.MapFrom(x => JObject.Parse(x.Form)))
-                .ForMember(x => x.Name, cfg => cfg.Ignore()); //Temporary shit
+                .ForMember(x => x.Name, cfg => cfg.ResolveUsing(Constructors.PaymentTemplateModelName));
+        }
+
+        private static class Constructors
+        {
+            public static string PaymentTemplateModelName(PaymentTemplate obj)
+            {
+                var sb = new StringBuilder();
+                var category = obj.Category;
+                while (category.ParentCategory.ParentCategory != null)
+                {
+                    sb.Insert(0, category.Name);
+                    if (category.ParentCategory != null)
+                        sb.Insert(0, " / ");
+                    category = category.ParentCategory;
+                }
+                return sb.ToString();
+            }
         }
     }
 }
