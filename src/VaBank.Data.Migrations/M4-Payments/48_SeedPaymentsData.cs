2cbb9641c44c32cfc177f5d1939cca3c2392d963
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-12-09 16:15:25 +0300

    Payment data without User payment profiles

--- src/VaBank.Data.Migrations/M4-Payments/48_SeedPaymentsData.cs
+++ src/VaBank.Data.Migrations/M4-Payments/48_SeedPaymentsData.cs
@@ -1,4 +1,12 @@
-﻿using FluentMigrator;
+﻿using Dapper;
+using FluentMigrator;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
 
 namespace VaBank.Data.Migrations
 {
@@ -6,6 +14,8 @@ namespace VaBank.Data.Migrations
     [Tags("Development", "Test", "Production")]
     public class SeedPaymentsData : Migration
     {
+        private const string VaBankPrefix = "3014"; 
+        
         public override void Down()
         {            
         }
@@ -13,6 +23,9 @@ namespace VaBank.Data.Migrations
         public override void Up()
         {
             SeedCategoriesTree();
+            SeedCorrespondentAccounts();
+            SeedPaymentTemplates();
+            SeedPaymentOrderTemplates();
         }
 
         private void SeedCategoriesTree()
@@ -26,15 +39,62 @@ namespace VaBank.Data.Migrations
                 .Row(new { Code = "PAYMENT-CELL-VELCOM-PHONENO", Name = new ExplicitUnicodeString("По номеру телефона"), Parent = "PAYMENT-CELL-VELCOM" });
         }
 
+        private void SeedCorrespondentAccounts()
+        {
+            Execute.WithConnection((connection, transaction) => 
+            {
+                var codes = connection.Query<string>("SELECT [Code] FROM [Accounting].[Bank] WHERE [Code] <> '153001966'", null, transaction).ToList();
+                foreach (var code in codes)
+                {
+                    var accountNo = string.Format("{0}{1}", VaBankPrefix, code);
+                    connection.Execute("INSERT INTO [Accounting].[Account] ([AccountNo],[CurrencyISOName],[OpenDateUtc],[ExpirationDateUtc],[Type]) VALUES (@AccountNo,@CurrencyISOName,@OpenDateUtc,@ExpirationDateUtc,@Type)",
+                        new { AccountNo = accountNo, CurrencyISOName = "BYR", OpenDateUtc = DateTime.UtcNow, ExpirationDateUtc = DateTime.UtcNow.AddYears(1), Type = "CorrespondentAccount" }, transaction);
+                    connection.Execute("INSERT INTO [Accounting].[CorrespondentAccount] ([AccountNo],[BankCode]) VALUES (@AccountNo,@BankCode)", new { AccountNo = accountNo, BankCode = code }, transaction);
+                }
+            });
+        }
+
         private void SeedPaymentTemplates()
         {
-            
+            var assembly = Assembly.GetExecutingAssembly();            
+            Insert.IntoTable("PaymentTemplate").InSchema("Payments")
+                .Row(new { Code = "PAYMENT-CELL-VELCOM-PHONENO", FormTemplate = new ExplicitUnicodeString(JObject.Parse(new StreamReader(assembly.GetManifestResourceStream("VaBank.Data.Migrations.M4_Payments.Templates.cell-velcom-phoneno.json")).ReadToEnd()).ToString(Formatting.None)) })
+                .Row(new { Code = "PAYMENT-CUSTOM-PAYMENTORDER", FormTemplate = new ExplicitUnicodeString(JObject.Parse(new StreamReader(assembly.GetManifestResourceStream("VaBank.Data.Migrations.M4_Payments.Templates.custom-paymentorder.json")).ReadToEnd()).ToString(Formatting.None)) });
         }
 
         private void SeedPaymentOrderTemplates()
         {
-            Insert.IntoTable("PaymentOrderTemplate").InSchema("Payment")
-                .Row(new {});
+            Insert.IntoTable("PaymentOrderTemplate").InSchema("Payments")
+                .Row(new 
+                {
+                    PaymentTemplateCode = "PAYMENT-CELL-VELCOM-PHONENO",
+                    PayerName = "{{payer.name}}",
+                    PayerTIN = "{{payer.tin}}",
+                    PayerBankCode = "{{payer.bankCode}}",
+                    PayerAccountNo = "{{payer.accountNo}}",
+                    BeneficiaryName =  new ExplicitUnicodeString("Унитарное предприятие по оказанию услуг \"Велком\""),
+                    BeneficiaryBankCode = "153001749",
+                    BeneficiaryAccountNo = "3012202410089",
+                    BeneficiaryTIN = "101528843",
+                    Amount = "{{form.amount}}",
+                    Purpose = new ExplicitUnicodeString("Пополнение счета. Номер телефона: {{form.phoneNo}}"),
+                    CurrencyISOName = "BYR"
+                })
+                .Row(new 
+                {
+                    PaymentTemplateCode = "PAYMENT-CUSTOM-PAYMENTORDER",
+                    PayerName = "{{payer.name}}",
+                    PayerTIN = "{{payer.tin}}",
+                    PayerBankCode = "{{payer.bankCode}}",
+                    PayerAccountNo = "{{payer.accountNo}}",
+                    BeneficiaryName =  "{{form.beneficiaryName}}",
+                    BeneficiaryBankCode = "{{form.beneficiaryBankCode}}",
+                    BeneficiaryAccountNo = "{{form.beneficiaryAccountNo}}",
+                    BeneficiaryTIN = "{{form.beneficiaryTIN}}",
+                    Amount = "{{form.amount}}",
+                    Purpose = "{{form.purpose}}",
+                    CurrencyISOName = "BYR"
+                });
         }
 
     }
