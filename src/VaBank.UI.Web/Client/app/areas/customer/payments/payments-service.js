4f7d4397616b9059f5a35bfb86c436cbd3f9238d
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2014-12-09 21:19:22 +0300

    Started implementing form creation from template.

--- src/VaBank.UI.Web/Client/app/areas/customer/payments/payments-service.js
+++ src/VaBank.UI.Web/Client/app/areas/customer/payments/payments-service.js
@@ -9,7 +9,28 @@
 
     function paymentService($q, $resource, $http, validationService, myCardsService) {
 
+        var Payment = $resource('/api/payments/:code', { code: '@code' }, {            
+           getTemplate: {
+               url: '/api/payments/:code/template',
+               method: 'GET'
+           } 
+        });
+
         var PaymentForm = (function () {
+            var create = function (formTemplate) {
+                var form = {};
+                var validators = {};
+                _.each(formTemplate, function(template, key) {
+                    form[key] = template.value;
+                    if (_.isObject(template.editor.validation)) {
+                        validators[key] = template.editor.validation;
+                    }
+                });
+                return {
+                    form: form,
+                    validators: validators
+                };
+            };
             var validate = function (templateCode, form) {
                 return $http({
                     method: 'POST',
@@ -20,14 +41,16 @@
                 });
             };
             return {                
-              validate: validate  
+                validate: validate,
+                create: create
             };
         })();
 
         return {
             Card: myCardsService.Card,
             SecurityCode: myCardsService.SecurityCode,
-            PaymentForm: PaymentForm
+            PaymentForm: PaymentForm,
+            Payment: Payment
         };
 
     }
