b6902b4d7f51c031f8a6906e77a03fb5e8c641f5
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-12-11 16:00:31 +0300

    Payments data finished

--- src/VaBank.Data.Migrations/M4-Payments/48_SeedPaymentsData.cs
+++ src/VaBank.Data.Migrations/M4-Payments/48_SeedPaymentsData.cs
@@ -7,6 +7,8 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Resources;
+using System.Text;
+using System.Collections.Generic;
 
 namespace VaBank.Data.Migrations
 {
@@ -26,6 +28,7 @@ namespace VaBank.Data.Migrations
             SeedCorrespondentAccounts();
             SeedPaymentTemplates();
             SeedPaymentOrderTemplates();
+            SeedUserPaymentProfiles();
         }
 
         private void SeedCategoriesTree()
@@ -97,5 +100,97 @@ namespace VaBank.Data.Migrations
                 });
         }
 
+        private void SeedUserPaymentProfiles()
+        {
+            Execute.WithConnection((connection, transaction) =>
+            {
+                var userProfiles = connection.Query<PartialUserProfile>("SELECT [UserID],[FirstName],[LastName] FROM [Membership].[UserProfile]", null, transaction).ToList();
+                var num = 1;
+                foreach (var userProfile in userProfiles)
+                {
+                    var param = new
+                    {
+                        UserId = userProfile.UserID,
+                        FullName = userProfile.GetFullName(),
+                        Address = "Minsk, Belarus",
+                        PayerTIN = GenerateTIN(ref num)
+                    };
+                    connection.Execute("INSERT INTO [Payments].[UserPaymentProfile] ([UserId],[FullName],[Address],[PayerTIN]) VALUES (@UserId,@FullName,@Address,@PayerTIN)", param, transaction);
+                    ++num;
+                }
+            });
+        }
+
+        private string GenerateTIN(ref int number)
+        {
+            if ((number / 100000000) > 1)
+                throw new InvalidOperationException("All TINs are already in use.");
+
+            var sb = new StringBuilder();
+            var coffs = new[] { 23, 19, 17, 13, 7, 5, 3 };
+            var dict = new Dictionary<int, char>
+            {
+                {0, 'A'},
+                {1, 'B'},
+                {2, 'C'},
+                {3, 'E'},
+                {4, 'H'},
+                {5, 'K'},
+                {6, 'M'},
+                {7, 'O'},
+                {8, 'P'},
+                {9, 'T'}
+            };
+
+            while (true)
+            {
+                sb.Append("A");
+                var str = number.ToString();
+                for (int i = 0; i < 7 - str.Length; i++)
+                {
+                    sb.Append('0');
+                }
+                sb.Append(str);
+
+                str = sb.ToString();
+                var sum = 10 * 29;
+
+                for (int i = 1; i < str.Length; i++)
+                {
+                    sum += coffs[i - 1] * (int)char.GetNumericValue(str[i]);
+                }
+                sum %= 11;
+
+                if (sum == 10)
+                {
+                    ++number;
+                    if ((number / 100000000) > 1)
+                        throw new InvalidOperationException("All TINs are already in use.");
+                    sb.Clear();
+                    continue;
+                }
+                else
+                {
+                    str = sb.ToString();
+                    sb.Append(sum.ToString());
+                    sb.Replace(str[1], dict[sum], 1, 1);
+                    break;
+                }
+            }        
+
+            return sb.ToString();
+        }
+
+        private class PartialUserProfile
+        {
+            public Guid UserID { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string GetFullName()
+            {
+                return string.Format("{0} {1}", FirstName, LastName);
+            }
+        }
     }
 }
