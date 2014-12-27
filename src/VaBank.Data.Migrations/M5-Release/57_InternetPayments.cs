2125e11af5a42356c75b3cdd12f0dfcbd20a999d
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-12-27 19:11:49 +0300

    Internet payments: in progress

---
+++ src/VaBank.Data.Migrations/M5-Release/57_InternetPayments.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentMigrator;
+
+namespace VaBank.Data.Migrations
+{
+    [Migration(57, "Add Internet payments.")]
+    [Tags("Development", "Test", "Production")]
+    public class InternetPayments : Migration
+    {
+        public override void Up()
+        {
+            SeedCategories();
+        }
+
+        public override void Down()
+        {
+        }
+
+        private void SeedCategories()
+        {
+            Insert.IntoTable("OperationCategory").InSchema("Accounting")
+                .Row(
+                    new
+                    {
+                        Code = "PAYMENT-INTERNET",
+                        Name = new ExplicitUnicodeString("Интернет"),
+                        Parent = "PAYMENT"
+                    });
+        }
+    }
+}
