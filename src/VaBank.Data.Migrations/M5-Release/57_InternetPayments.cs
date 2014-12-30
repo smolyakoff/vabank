46f1ec40f0d218ea338bfd6707b2a799272ac718
Zakhar Makarevich <zakharmakarevich@gmail.com>
2014-12-30 18:48:22 +0300

    Byfly internet payment data

--- src/VaBank.Data.Migrations/M5-Release/57_InternetPayments.cs
+++ src/VaBank.Data.Migrations/M5-Release/57_InternetPayments.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using FluentMigrator;
+﻿using FluentMigrator;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace VaBank.Data.Migrations
 {
@@ -10,6 +11,8 @@ namespace VaBank.Data.Migrations
         public override void Up()
         {
             SeedCategories();
+            SeedPaymentTemplates();
+            SeedPaymentOrderTemplates();
         }
 
         public override void Down()
@@ -26,6 +29,50 @@ namespace VaBank.Data.Migrations
                         Name = new ExplicitUnicodeString("Интернет"),
                         Parent = "PAYMENT"
                     });
+            Insert.IntoTable("OperationCategory").InSchema("Accounting")
+                .Row(
+                    new
+                    {
+                        Code = "PAYMENT-INTERNET-BYFLY",
+                        Name = "BYFLY",
+                        Parent = "PAYMENT-INTERNET"
+                    });
+        }
+
+        private void SeedPaymentTemplates()
+        {
+            Insert.IntoTable("PaymentTemplate").InSchema("Payments")
+                .Row(new
+                {
+                    Code = "PAYMENT-INTERNET-BYFLY",
+                    FormTemplate =
+                        new ExplicitUnicodeString(
+                            JObject.Parse(Resource.ReadToEnd("M5_Release/Templates/internet-byfly.json"))
+                                .ToString(Formatting.None)),
+                    InfoTemplate =
+                        new ExplicitUnicodeString(Resource.ReadToEnd("M5_Release/Templates/internet-byfly.info.txt")),
+                    DisplayName = "BYFLY"
+                });
+        }
+
+        private void SeedPaymentOrderTemplates()
+        {
+            Insert.IntoTable("PaymentOrderTemplate").InSchema("Payments")
+                .Row(new
+                {
+                    PaymentTemplateCode = "PAYMENT-INTERNET-BYFLY",
+                    PayerName = "{{form.payerName}}",
+                    PayerTIN = "{{form.payerTin}}",
+                    PayerBankCode = "{{form.payerBankCode}}",
+                    PayerAccountNo = "{{form.payerAccountNo}}",
+                    BeneficiaryName = new ExplicitUnicodeString("Республиканское унитарное предприятие \"Белтелеком\""),
+                    BeneficiaryBankCode = "153001749",
+                    BeneficiaryAccountNo = "3012016182011",
+                    BeneficiaryTIN = "191124936",
+                    Amount = "{{form.amount}}",
+                    Purpose = new ExplicitUnicodeString("Пополнение счета BYFLY. Номер счёта: {{form.accountNo}}"),
+                    CurrencyISOName = "BYR"
+                });
         }
     }
 }
