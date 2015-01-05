194cb54036cb12a595b7ba82335730553e9ab24a
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2015-01-05 11:29:43 +0300

    Fixed bug with internet payment display name.

---
+++ src/VaBank.Data.Migrations/M5-Release/58_FixedUnicodeForTemplateName.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace VaBank.Data.Migrations
+{
+    [Migration(58, "Fixed bug with display name of byfly payment.")]
+    [Tags("Development", "Test", "Production")]
+    public class FixedUnicodeForTemplateName : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"UPDATE [Payments].[PaymentTemplate]
+                          SET [DisplayName] = N'Интернет - BYFLY'
+                          WHERE [Code] = 'PAYMENT-INTERNET-BYFLY'");
+        }
+
+        public override void Down()
+        {
+            //do nothing
+        }
+    }
+}
